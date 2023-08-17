a603e1ae0048b6009ef88ffeaaa521b85ccba007
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-16 22:28:36 -0300

    Commit

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -10,11 +10,18 @@ terraform {
     }
   }
 
+  backend s3 {
+    bucket = "tf-terraform-hello-world-state-thomas-fernandes"
+    key = "terraform.tfstate"
+    dynamodb_table = "tf-terraform-hello-world-state-v1"
+    region = "us-east-1"
+  }
+
 }
 
 provider "aws" {
   region                   = "us-east-1"
-  shared_config_files      = [".aws/config"]
-  shared_credentials_files = [".aws/credentials"]
-  profile                  = "fiap-iac"
+ # shared_config_files      = [".aws/config"]
+ # shared_credentials_files = [".aws/credentials"]
+ # profile                  = "fiap-iac"
 }
